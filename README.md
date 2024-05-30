45b97b0a60c108b83ed64281c2fa643b43cec459
Jonas Tibisch <99822898+ScorpIsTheGoat@users.noreply.github.com>
2024-05-30 13:42:25 +0200

    User is able to upload file. Next step, conversion

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # converter
-Website with backend which let's the user create and account and convert codecs and formats, add subtitles to videos in any language to english and do colorgrading/upscaling.
+## Idee
+In diesem Projekt ist das Ziel darin, eine über das Internet zugängliche Website zu erstellen, auf der der Benutzer:
+- convert video codecs (example: H.264 --> DNxHD) 
+- convert data types (example: mp4 --> mov)
+- add subtitles (creation of srt file, aswell as inbedding in video)
+- upscaling of video/picutres (example: from HD to 4K)
+- colorgrading of video (example: darker, higher contrast)
+## Requirements
+Das FrontEnd werde ich standard mässig mit HTML, CSS und JavaScript programmieren, beim BackEnd benutzte ich FastAPI, ein Framework für Python, welches einfach zu benutzten ist. Für die konvertierung bietet sich ffmpeg-python an, damit habe ich bereits Erfahrung. Für die Untertitel und das ColorGrading/Upscaling werde ich Open-AI verwenden. Falls möglich würde ich die erstellten Benutzer in einer SQL-Datenbank speichern.
+## Nächste Schritte
+1. Konvertierung der Hochgeladenen Videos und das zurück senden an FrontEnd(Hochladen funktioniert schon)
+2. Implementierung eines Ladebalkens
+3. User-Login erstellen
+4. ColorGrading/Untertitel implementieren
+
---
+++ main.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI, File, Form, UploadFile, Request, HTTPException
+import uvicorn
+from enum import Enum
+from fastapi.responses import JSONResponse, FileResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.encoders import jsonable_encoder
+from pathlib import Path
+from pydantic import BaseModel
+import os
+#import whisper
+import shutil
+import time
+import ffmpeg
+
+class FileProperties:
+    def __init__(self, file_path):
+        self.file_name = Path(file_path).stem
+        self.file_type = Path(file_path).suffix
+        self.file_size = format_file_size(os.path.getsize(file_path))
+        self.file_access_time = time.ctime(os.path.getatime(file_path))
+class SelectValue(BaseModel):
+    filetype: str
+def file_exists(path: str):
+    file = Path(path)
+    if file.exists():
+        return True
+    else:
+        return False
+def format_file_size(size_bytes):
+    if size_bytes < 1024:
+        return f"{size_bytes} B"
+    elif size_bytes < 1024 ** 2:
+        return f"{size_bytes / 1024:.2f} KB"
+    elif size_bytes < 1024 ** 3:
+        return f"{size_bytes / (1024 ** 2):.2f} MB"
+    else:
+        return f"{size_bytes / (1024 ** 3):.2f} GB"
+#create instance
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=["*"],  
+)
+#model = whisper.load_model("medium")
+UPLOAD_DIR = Path() / 'uploads'
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    context = {"request" : request}
+    return templates.TemplateResponse("index.html", context)
+
+@app.post("/upload")
+async def create_upload_file(file: UploadFile):
+    files = os.listdir(UPLOAD_DIR)
+
+    for file_name in files:
+            file_path = os.path.join(UPLOAD_DIR, file_name)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+    print("Has been deleted")
+    data = await file.read()
+    global save_to
+    save_to = UPLOAD_DIR / file.filename
+    with open(save_to, 'wb') as f:
+        f.write(data)
+        print("finished uploading")
+    while True:
+        if file_exists(save_to) and os.access(save_to, os.R_OK):
+            break
+    global uploaded_file
+    uploaded_file = FileProperties(save_to)
+    print(uploaded_file.file_name, uploaded_file.file_type, uploaded_file.file_size, uploaded_file.file_access_time)
+
+    return {"filename": file.filename}
+
+@app.post("/convert")
+async def convert_file(select_value: SelectValue):
+    filetype = select_value.filetype
+    original_file_path = save_to
+    converted_file_name = f'converted_file_{uploaded_file.file_name}.{filetype}'
+    print(filetype)
+    print(original_file_path)
+    print(converted_file_name)
+    
+    try:       
+        
+        return FileResponse(path=converted_file_path, filename=f"converted_file.{filetype}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
