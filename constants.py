fdaba05bb601704ceed480446c46ef57c1e7662a
Jonas Tibisch <99822898+ScorpIsTheGoat@users.noreply.github.com>
2024-10-22 19:50:57 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from database import *
+from enum import Enum
+from pathlib import Path
+
+
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SALT = b'$2b$12$wDSmrpih/oSxFOEtUasove'
+UPLOAD_DIR = Path() / 'uploads'
